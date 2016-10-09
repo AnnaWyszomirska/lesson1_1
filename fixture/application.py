e1ade46bf0ed6e178ab212077ef8f2eba8d8629d
AnnaWyszomirska <aniawzs@wp.pl>
2016-10-09 15:09:55 +0200

    Wyodrębniona klasa do kierowania sesjami

--- fixture/application.py
+++ fixture/application.py
@@ -1,25 +1,18 @@
 from selenium import webdriver
+from fixture.session import SessionHelper
+
 
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Chrome(executable_path="C:\\chromedriver_win32\\chromedriver.exe")
         self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/addressbook/group.php")
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_groups_page(self):
         wd = self.wd
@@ -52,9 +45,6 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("group page").click()
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Wyloguj się").click()
 
     def destroy(self):
         self.wd.quit()
