4560d009c7ed41f5cfe159ee443c0dfd93165dc6
AnnaWyszomirska <aniawzs@wp.pl>
2016-10-17 10:59:26 +0200

    modify group - refactoring

--- tests/test_change_in_the_group.py
+++ tests/test_change_in_the_group.py
@@ -1,7 +1,14 @@
 
 from model.group import Group
 
-def test_change_in_the_group(app):
+def test_change_in_the_group_header(app):
     app.session.login(username="admin", password="secret")
-    app.group.change_group(Group(name="changed name", header="changed header", footer="changed footer"))
+    app.group.change_group(Group(header="changed header"))
+    app.session.logout()
+
+
+
+def test_change_in_the_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.change_group(Group(name="changed name"))
     app.session.logout()
