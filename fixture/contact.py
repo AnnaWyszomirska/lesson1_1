69ac47d21c395338069ad73304d0c5c2a68e567d
AnnaWyszomirska <aniawzs@wp.pl>
2016-10-24 21:36:57 +0200

    cache changes

--- fixture/contact.py
+++ fixture/contact.py
@@ -20,6 +20,7 @@ class ContactHelper:
         self.fill_contact_form(contact)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.return_to_homepage()
+        self.contact_cache = None
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
@@ -70,6 +71,7 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
         self.return_to_homepage()
+        self.contact_cache = None
 
     def change_contact(self, contact):
         wd = self.app.wd
@@ -79,7 +81,7 @@ class ContactHelper:
         self.fill_contact_form(contact)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.return_to_homepage()
-
+        self.contact_cache = None
 
     def select_first_contact(self):
         wd = self.app.wd
@@ -94,16 +96,18 @@ class ContactHelper:
         self.open_home_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    contact_cache = None
     def get_contact_list(self):
-        wd = self.app.wd
-        self.open_home_page()
-        contacts = []
-        for element in wd.find_elements_by_name("entry"):
-            cells = element.find_elements_by_tag_name("td")
-            text1 = cells[1].text
-            text2 = cells[2].text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            contacts.append(Contact(firstname= text2, lastname= text1, id = id))
-        return contacts
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.open_home_page()
+            self.contact_cache = []
+            for element in wd.find_elements_by_name("entry"):
+                cells = element.find_elements_by_tag_name("td")
+                text1 = cells[1].text
+                text2 = cells[2].text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.contact_cache.append(Contact(firstname= text2, lastname= text1, id = id))
+            return list(self.contact_cache)
 
 
--- tests/test_add_new_contact.py
+++ tests/test_add_new_contact.py
@@ -18,7 +18,7 @@ def test_add_contact(app):
                             address2="My second address ", privatephone="23415257354735",
                             comments="Brak uwag")
     app.contact.add(contact)
+    assert len(old_contacts) + 1 == app.contact.count_contact()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key = Contact.id_or_max) == sorted(new_contacts, key = Contact.id_or_max)
