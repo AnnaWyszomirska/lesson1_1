fd41bb6a118eb360b5a844d1467009c2deaa90ee
AnnaWyszomirska <aniawzs@wp.pl>
2016-11-03 21:20:31 +0100

    test contact fields on homepage

--- fixture/contact.py
+++ fixture/contact.py
@@ -129,10 +129,10 @@ class ContactHelper:
                 text1 = cells[1].text
                 text2 = cells[2].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
-                adres = cells[3].text
+                address = cells[3].text
                 all_emails = cells[4].text
                 all_phones = cells[5].text
-                self.contact_cache.append(Contact(firstname= text2, lastname= text1, id = id, address= adres,
+                self.contact_cache.append(Contact(firstname= text2, lastname= text1, id = id, address= address,
                                                   all_emails_form_home_page= all_emails,  all_phones_from_home_page = all_phones))
         return list(self.contact_cache)
 
@@ -142,12 +142,16 @@ class ContactHelper:
         self.open_contact_to_edit_by_index(index)
         firstname = wd.find_element_by_name("firstname").get_attribute("value")
         lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        address = wd.find_element_by_name("address").get_attribute("value")
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
         id = wd.find_element_by_name("id").get_attribute("value")
         home = wd.find_element_by_name("home").get_attribute("value")
         mobile = wd.find_element_by_name("mobile").get_attribute("value")
         work = wd.find_element_by_name("work").get_attribute("value")
         privatephone = wd.find_element_by_name("phone2").get_attribute("value")
-        return Contact(firstname = firstname, lastname= lastname, id = id, home =home,
+        return Contact(firstname = firstname, lastname= lastname, id = id, home =home, address= address, email=email, email2=email2, email3= email3,
                        mobile=mobile, work = work, privatephone=privatephone )
 
     def get_contact_from_view_page(self, index):
--- tests/test_contact_fields.py
+++ tests/test_contact_fields.py
@@ -1,9 +1,28 @@
-
+import re
+from random import randrange
 
 def test_contact_on_the_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
 
-    assert contact_from_home_page.all_emails_form_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.address == contact_from_edit_page.address
+    assert contact_from_home_page.all_emails_form_home_page == merge_emails_like_on_homepage(contact_from_edit_page)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_homepage(contact_from_edit_page)
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+def merge_emails_like_on_homepage(contact):
+    return "\n".join(filter(lambda x: x != "",(filter(lambda x: x is not None,[contact.email, contact.email2, contact.email3]))))
+
+def merge_phones_like_on_homepage(contact):
+    return "\n".join(filter(lambda x: x != "",
+                                map(lambda x: clear(x),
+                                    filter(lambda x: x is not None,
+                                           [contact.home, contact.mobile, contact.work, contact.privatephone]))))
+
 
