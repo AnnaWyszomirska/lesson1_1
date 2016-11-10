8470de2827dac1e321407e4f5b28908cb5ee6744
AnnaWyszomirska <aniawzs@wp.pl>
2016-11-10 12:42:00 +0100

    pytest generate tests function changes

---
+++ data/contacts.py
@@ -0,0 +1,7 @@
+from model.contact import Contact
+
+testdata = [
+    Contact (firstname="Joanna", lastname="Testowa", bday="1",bmonth="Styczeń"),
+    Contact(firstname="Basia", lastname="Kowalska", bday="2", bmonth="Luty"),
+]
+
--- tests/test_add_new_contact.py
+++ tests/test_add_new_contact.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from model.contact import Contact
-import pytest
-from data.add_contact import constant as testdata
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contact):
+    contact = json_contact
     old_contacts = app.contact.get_contact_list()
     app.contact.add(contact)
     assert len(old_contacts) + 1 == app.contact.count_contact()
